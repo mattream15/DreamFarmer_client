2ae8b6409c972f6b89674d931014878b0b9428ae
Matthew Ream <matthewtimothyream@gmail.com>
2020-01-16 13:44:24 -0600

    transitioning to grow rooms

---
+++ src/adapters/GrowRoomsAdapter.js
@@ -0,0 +1,44 @@
+class GrowRoomsAdapter {
+    constructor() {
+        this.baseUrl = 'http:localhost:3000/api/v1/grow_rooms'
+    }
+
+    getGrowRooms() {
+        return fetch(this.baseUrl).then(res => res.json()
+        )
+    }
+
+    createGrowRoom(value1, value2, value3, value4, value5) {
+        const growRoom = {
+            lightSource: value1,
+            nutrient: value2,
+            relativeHumidity: value3,
+            temperature: value4,
+            pH: value5
+        }
+        return fetch(this.baseUrl, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({ growRoom }),
+        }).then(res => res.json())
+    }
+
+    updateGrowRoom(value1, value2, value3, value4, value5, id) {
+        const growRoom = {
+            lightSource: value1,
+            nutrient: value2,
+            relativeHumidity: value3,
+            temperature: value4,
+            pH: value5
+        }
+        return fetch(`${this.baseUrl}/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({ growRoom }),
+        }).then(res => res.json())
+    }
+}
